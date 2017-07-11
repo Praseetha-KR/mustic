87caa3a5720abab59972adee9dce1943629a9726
Praseetha-KR <praseetha04@gmail.com>
2017-07-12 00:36:55 +0530

    Adds gulp

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+const gulp = require('gulp');
+const watchify = require('watchify');
+const browserify = require('gulp-browserify');
+const rename = require('gulp-rename');
+const scss = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const gutil = require('gulp-util');
+const babel = require('gulp-babel');
+const cssnano = require('gulp-cssnano');
+
+const srcFolder = './src/';
+const srcFile = 'index.js';
+const destFolder = './build/';
+const destJSFile = 'mustic.min.js';
+const destCSSFile = 'mustic.min.css';
+
+gulp.task('js', () => {
+    gulp
+        .src([srcFolder + srcFile])
+        .pipe(
+            browserify({
+                cache: {}
+            })
+        )
+        .pipe(
+            babel({
+                presets: ['es2015']
+            })
+        )
+        // .pipe(uglify())
+        .on('error', function(err) {
+            gutil.log(gutil.colors.red('[Error]'), err.toString());
+        })
+        .pipe(rename(destJSFile))
+        .pipe(gulp.dest(destFolder));
+});
+
+gulp.task('scss', () => {
+    gulp
+        .src(srcFolder + '**/*.scss')
+        .pipe(scss().on('error', scss.logError))
+        .pipe(cssnano())
+        .pipe(rename(destCSSFile))
+        .pipe(gulp.dest(destFolder));
+});
+
+gulp.task('watch', () => {
+    gulp.watch(srcFolder + '**/*.js', ['js']);
+    gulp.watch(srcFolder + '**/*.scss', ['scss']);
+});
+
+gulp.task('default', ['js', 'scss', 'watch']);
