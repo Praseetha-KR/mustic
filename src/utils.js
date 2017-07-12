adab560ce748e03758d57eaf6f3b0f04aa78c203
Praseetha-KR <praseetha04@gmail.com>
2017-07-12 19:28:11 +0530

    Adds assertions on util fns

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,13 @@
+const assert = require('assert');
+
 /**
  * Converts a fraction to a whole number
  * @param {Number} fraction
  */
 function wholeNumber(fraction) {
-    return Math.abs(Math.floor(fraction));
+    assert.equal(typeof fraction, 'number');
+
+    return Math.abs(Math.round(fraction));
 }
 
 /**
@@ -13,6 +17,11 @@ function wholeNumber(fraction) {
  * @param {Array} rgb - rgb is an array of size 3 with value range is [0-255]
  */
 function percievedContrast(rgb) {
+    assert(Array.isArray(rgb));
+    assert.equal(rgb.length, 3);
+    assert.equal(rgb.filter(v => typeof v !== 'number').length, 0);
+    assert.equal(rgb.filter(v => v < 0 && v > 255).length, 0);
+
     const brightness = Math.round(
         (parseInt(rgb[0]) * 299 +
             parseInt(rgb[1]) * 587 +
