8cfe011543e4bdd59960c20a461abf465b9b8db6
Praseetha-KR <praseetha04@gmail.com>
2017-07-12 18:08:54 +0530

    Adds exception handling in mustic index

--- src/mustic/index.js
+++ src/mustic/index.js
@@ -2,11 +2,17 @@ const Dom = require('./Dom');
 const Music = require('./Music');
 
 function init(root) {
-    const dom = new Dom(root);
-    const music = new Music();
+    try {
+        const dom = new Dom(root);
+        const music = new Music();
 
-    dom.listenBtnClick(music);
-    dom.listenDeviceOrientation(music);
+        dom.listenBtnClick(music);
+        dom.listenDeviceOrientation(music);
+
+        return { dom, music };
+    } catch (e) {
+        throw e;
+    }
 }
 
 module.exports = init;
