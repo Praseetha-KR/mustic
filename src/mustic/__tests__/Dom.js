41c8a03f13b11f75a1ce0b885f9820545730e7d4
Praseetha-KR <praseetha04@gmail.com>
2017-07-12 23:56:27 +0530

    Adds unit tests for Dom

---
+++ src/mustic/__tests__/Dom.js
@@ -0,0 +1,21 @@
+describe('Dom class', () => {
+    test('should', () => {
+        const Dom = require('../Dom');
+        const dom = new Dom(document.createElement('div'));
+
+        expect(dom.isChecked).toBeDefined();
+
+        dom.theme = { background: 'rgb(0, 0, 0)', foreground: 'white' };
+        expect(dom.container.style.backgroundColor).toEqual('rgb(0, 0, 0)');
+        expect(dom.container.style.color).toEqual('white');
+
+        dom.displayFrequency = '1000';
+        expect(dom.display.innerHTML).toEqual('1000 Hz');
+
+        dom.setPlayLabel();
+        expect(dom.label.innerHTML).toEqual('► Play');
+
+        dom.setPauseLabel();
+        expect(dom.label.innerHTML).toEqual('▌▌ Pause');
+    });
+});
