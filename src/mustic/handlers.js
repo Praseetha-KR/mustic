e715ece56144a2f824c3361ab846102b66d44974
Praseetha-KR <praseetha04@gmail.com>
2017-07-13 09:57:00 +0530

    Remove assertions from handlers, Add default value for orientation

--- src/mustic/handlers.js
+++ src/mustic/handlers.js
@@ -1,29 +1,19 @@
-const assert = require('assert');
-const Dom = require('./Dom');
-const Music = require('./Music');
 const transforms = require('./transforms');
 
 function handleOrientation(music, event) {
-    assert(this instanceof Dom);
-    assert(music instanceof Music);
-    assert(event instanceof Event);
-
-    const alpha = event.alpha;
-    const beta = event.beta;
-    const gamma = event.gamma;
+    const alpha = event.alpha || 0;
+    const beta = event.beta || 0;
+    const gamma = event.gamma || 0;
 
     const theme = transforms.orientationToTheme(alpha, beta, gamma);
     const freq = transforms.orientationToMusic(alpha, beta, gamma);
 
-    this.theme = theme;
-    this.displayFrequency = freq;
+    this.setTheme(theme);
+    this.setDisplayFrequency(freq);
     music.frequency = freq;
 }
 
 function handleClick(music) {
-    assert(this instanceof Dom);
-    assert(music instanceof Music);
-
     if (this.isChecked) {
         music.play();
         this.setPauseLabel();
