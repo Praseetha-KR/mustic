45d5aeeeb97dbf0a58b6cc6e95eaf347f7806fdb
Praseetha-KR <praseetha04@gmail.com>
2017-07-13 09:55:42 +0530

    Renames to instance & update theme & freq fns in Dom test

--- src/mustic/__tests__/Dom.js
+++ src/mustic/__tests__/Dom.js
@@ -1,21 +1,23 @@
 describe('Dom class', () => {
     test('should provide methods', () => {
         const Dom = require('../Dom');
-        const dom = new Dom(document.createElement('div'));
+        const instance = new Dom(document.createElement('div'));
 
-        expect(dom.isChecked).toBeDefined();
+        expect(instance.isChecked).toBeDefined();
 
-        dom.theme = { background: 'rgb(0, 0, 0)', foreground: 'white' };
-        expect(dom.container.style.backgroundColor).toEqual('rgb(0, 0, 0)');
-        expect(dom.container.style.color).toEqual('white');
+        instance.setTheme({ background: 'rgb(0, 0, 0)', foreground: 'white' });
+        expect(instance.domobj.container.style.backgroundColor).toEqual(
+            'rgb(0, 0, 0)'
+        );
+        expect(instance.domobj.container.style.color).toEqual('white');
 
-        dom.displayFrequency = '1000';
-        expect(dom.display.innerHTML).toEqual('1000 Hz');
+        instance.setDisplayFrequency('1000');
+        expect(instance.domobj.display.innerHTML).toEqual('1000 Hz');
 
-        dom.setPlayLabel();
-        expect(dom.label.innerHTML).toEqual('► Play');
+        instance.setPlayLabel();
+        expect(instance.domobj.label.innerHTML).toEqual('► Play');
 
-        dom.setPauseLabel();
-        expect(dom.label.innerHTML).toEqual('▌▌ Pause');
+        instance.setPauseLabel();
+        expect(instance.domobj.label.innerHTML).toEqual('▌▌ Pause');
     });
 });
