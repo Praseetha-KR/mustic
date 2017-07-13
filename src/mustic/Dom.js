66ad285301cd55be2f504d94cd1e6048df92e74c
Praseetha-KR <praseetha04@gmail.com>
2017-07-13 09:54:57 +0530

    Adds a parent obj to Dom class

--- src/mustic/Dom.js
+++ src/mustic/Dom.js
@@ -3,54 +3,57 @@ const { handleOrientation, handleClick } = require('./handlers');
 
 class Dom {
     constructor(document) {
-        this.component = document;
-        this.component.innerHTML = `
-            <div class="${consts.app.BASE}">
-                <div class="${consts.app.BASE}__title">Mustic</div>
-                <div class="${consts.app.DISPLAY}"></div>
-                <input type="checkbox" id="playpause" class="${consts.app
-                    .BUTTON_INPUT}">
-                <label class="${consts.app
-                    .BUTTON_LABEL}" for="playpause">${consts.app
+        this.domobj = {};
+        this.domobj.component = document;
+        this.domobj.component.innerHTML = `
+                    <div class="${consts.app.BASE}">
+                        <div class="${consts.app.BASE}__title">Mustic</div>
+                        <div class="${consts.app.DISPLAY}"></div>
+                        <input type="checkbox" id="playpause" class="${consts
+                            .app.BUTTON_INPUT}">
+                        <label class="${consts.app
+                            .BUTTON_LABEL}" for="playpause">${consts.app
             .LABEL_PLAY}</label>
-            </div>
-        `;
-
-        const $ = this.component.querySelector.bind(this.component);
-
-        this.container = $(`.${consts.app.BASE}`);
-        this.display = $(`.${consts.app.DISPLAY}`);
-        this.label = $(`.${consts.app.BUTTON_LABEL}`);
-        this.input = $(`.${consts.app.BUTTON_INPUT}`);
-
-        this.container.style.backgroundColor = consts.theme.BACKGROUND;
-        this.container.style.color = consts.theme.FOREGROUND;
-        this.display.innerHTML = '0 Hz';
+                    </div>
+                `;
+        const $ = this.domobj.component.querySelector.bind(
+            this.domobj.component
+        );
+        this.domobj.container = $(`.${consts.app.BASE}`);
+        this.domobj.display = $(`.${consts.app.DISPLAY}`);
+        this.domobj.label = $(`.${consts.app.BUTTON_LABEL}`);
+        this.domobj.input = $(`.${consts.app.BUTTON_INPUT}`);
+        this.domobj.container.style.backgroundColor = consts.theme.BACKGROUND;
+        this.domobj.container.style.color = consts.theme.FOREGROUND;
+        this.domobj.display.innerHTML = '0 Hz';
     }
 
     get isChecked() {
-        return this.input.checked;
+        return this.domobj.input.checked;
     }
 
-    set theme(theme) {
-        this.container.style.backgroundColor = theme.background;
-        this.container.style.color = theme.foreground;
+    setDisplayFrequency(freq) {
+        this.domobj.display.innerHTML = `${freq} Hz`;
     }
 
-    set displayFrequency(freq) {
-        this.display.innerHTML = `${freq} Hz`;
+    setTheme(theme) {
+        this.domobj.container.style.backgroundColor = theme.background;
+        this.domobj.container.style.color = theme.foreground;
     }
 
     setPlayLabel() {
-        this.label.innerHTML = `${consts.icon.PLAY} Play`;
+        this.domobj.label.innerHTML = `${consts.icon.PLAY} Play`;
     }
 
     setPauseLabel() {
-        this.label.innerHTML = `${consts.icon.PAUSE} Pause`;
+        this.domobj.label.innerHTML = `${consts.icon.PAUSE} Pause`;
     }
 
     listenBtnClick(music) {
-        this.input.addEventListener('click', handleClick.bind(this, music));
+        this.domobj.input.addEventListener(
+            'click',
+            handleClick.bind(this, music)
+        );
     }
 
     listenDeviceOrientation(music) {
